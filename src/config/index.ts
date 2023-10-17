bd06107345529cfd7e909f4818e56e9b592e6139
phoax <raphael.pralat@consensys.net>
2023-10-17 16:26:00 +0200

    fix: verify script

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,8 +8,8 @@ dotenvConfig();
 export const config: Config = {
   L1_PROVIDER_URL: process.env.PROVIDER_URL || '',
   L2_PROVIDER_URL: process.env.LINEA_PROVIDER_URL || '',
-  L1_TOKEN_BRIDGE_ADDRESS: process.env.CONTRACT_ADDRESS || '',
-  L2_TOKEN_BRIDGE_ADDRESS: process.env.L2_CONTRACT_ADDRESS || '',
+  L1_TOKEN_BRIDGE_ADDRESS: process.env.CONTRACT_ADDRESS || '0x051F1D88f0aF5763fB888eC4378b4D8B29ea3319',
+  L2_TOKEN_BRIDGE_ADDRESS: process.env.L2_CONTRACT_ADDRESS || '0x353012dc4a9A6cF55c941bADC267f82004A8ceB9',
   TOKEN_BRIDGE_ABI_PATH: 'src/abis/token-bridge.abi.json',
   ERC20_ABI_PATH: 'src/abis/ERC20.abi.json',
   ERC20_BYTE32_ABI_PATH: 'src/abis/ERC20-byte32.abi.json',
